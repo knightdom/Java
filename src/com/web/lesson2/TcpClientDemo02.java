30a12e138b0fa5b9724cbfa962ea4af044d90e93
Edgar <kinghtdom@outlook.com>
2021-06-23 23:05:46 +0800

    学习Java的tcp通信

--- src/com/web/lesson2/TcpClientDemo02.java
+++ src/com/web/lesson2/TcpClientDemo02.java
@@ -1,8 +1,6 @@
 package com.web.lesson2;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -13,14 +11,29 @@ public class TcpClientDemo02 {
         // 2. 创建一个输出流
         OutputStream os = socket.getOutputStream();
         // 3. 读取文件
-        FileInputStream fis = new FileInputStream("test.jpg");
+        FileInputStream fis = new FileInputStream("test.jpg");  // 文件管道流
         // 4. 写出文件
         byte[] buffer = new byte[1024];
         int len;
         while((len=fis.read(buffer)) != -1) {
             os.write(buffer, 0, len);
         }
+
+        // 通知服务器，我已经结束了
+        socket.shutdownInput(); // 我已经传输完了
+
+        // 确认服务器接收完毕，才能断开连接
+        InputStream is = socket.getInputStream();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();   // 字节管道流
+        byte[] buffer2 = new byte[1024];
+        int len2;
+        while ((len2 = is.read(buffer2)) != -1) {
+            baos.write(buffer2, 0, len2);
+        }
+
         // 5. 关闭资源
+        baos.close();
+        is.close();
         fis.close();
         os.close();
         socket.close();
--- src/com/web/lesson2/TcpServerDemo02.java
+++ src/com/web/lesson2/TcpServerDemo02.java
@@ -1,4 +1,37 @@
 package com.web.lesson2;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class TcpServerDemo02 {
+    public static void main(String[] args) throws Exception {
+        // 1. 创建服务
+        ServerSocket serverSocket = new ServerSocket(9000);
+        // 2. 监听客户端的连接
+        Socket socket = serverSocket.accept();// 阻断式监听，会一直等待客户端连接
+        // 3. 获取输入流
+        InputStream is = socket.getInputStream();
+        // 4. 文件输出
+        FileOutputStream fos = new FileOutputStream("receive.jpg");
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = is.read(buffer)) != -1) {
+            fos.write(buffer, 0, len);
+        }
+
+        // 通知客户端接收完毕
+        OutputStream os = socket.getOutputStream();
+        os.write("接收完毕".getBytes(StandardCharsets.UTF_8));
+
+        // 关闭资源
+        fos.close();
+        is.close();
+        socket.close();
+        serverSocket.close();
+    }
 }
