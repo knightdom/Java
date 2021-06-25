7232000e1467f73dfa558181d4f86ec686d7506e
Edgar <kinghtdom@outlook.com>
2021-06-26 00:25:05 +0800

    完成udp单向聊天

--- src/com/web/chat/UdpReceiveDemo01.java
+++ src/com/web/chat/UdpReceiveDemo01.java
@@ -1,5 +1,6 @@
 package com.web.chat;
 
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
@@ -9,7 +10,18 @@ public class UdpReceiveDemo01 {
         while (true) {
             // 准备接收包裹
             byte[] container = new byte[1024];
-            
+            DatagramPacket packet = new DatagramPacket(container, 0, container.length);
+            socket.receive(packet); // 阻塞式接收包裹
+
+            byte[] data = packet.getData();
+            String receiveData = new String(data, 0, data.length);
+            System.out.println(receiveData);
+
+            // 断开连接-bye
+            if (receiveData.equals("bye")) {
+                break;
+            }
         }
+        socket.close();
     }
 }
